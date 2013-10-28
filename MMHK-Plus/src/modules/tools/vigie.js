d739ef043db08f5a656e9cbe200b506614a03bb0
Aendawyn <aendawyn@gmail.com>
2013-10-28 15:52:26 +0100

    Update Lookout module to work Add simplisitc heuristic to guess playerId

--- MMHK-Plus/src/modules/tools/vigie.js
+++ MMHK-Plus/src/modules/tools/vigie.js
@@ -4,6 +4,7 @@ MMHKPLUS.Lookout = MMHKPLUS.PanelElement.extend({
 	moves : [],
 	cachePlayers : {},
 	cacheAlliances : {},
+	cacheMoves : {},
     currentPlot : "",
 	
 	options : {
@@ -94,18 +95,29 @@ MMHKPLUS.Lookout = MMHKPLUS.PanelElement.extend({
 			{
 				return (!player.isInAlliance() 
 							|| (player.isInAlliance() && !filterAlliance) 
-							|| (player.isInAlliance() && filterAlliance && self._getPlayerInfo(item.masterHeroMove.playerId).allianceName != player.get("allianceName")))
-						&& ((self._getPlayerInfo(item.masterHeroMove.playerId).playerName.toUpperCase().indexOf(filterText) != -1)
+							/*|| (player.isInAlliance() && filterAlliance && self._getPlayerInfo(item.masterHeroMove.playerId).allianceName != player.get("allianceName"))*/)
+						/*&& ((self._getPlayerInfo(item.masterHeroMove.playerId).playerName.toUpperCase().indexOf(filterText) != -1)
 								|| (self._getPlayerInfo(item.masterHeroMove.playerId).allianceName.toUpperCase().indexOf(filterText) != -1)
-								|| (filterText == ""));
+								|| (filterText == ""))*/;
 			}
 		);
 		filterMoves.sort(function(m, n)
 			{
-				return m.masterHeroMove.endDate - n.masterHeroMove.endDate;
+				return m.endDate - n.endDate;
 			}
 		);
-
+		
+		var movementsToCheck = $.grep(filterMoves, function(item, index)
+			{
+				return !hasProperty(self.cacheMoves, item.id) ||
+						(hasProperty(self.cacheMoves, item.id) 
+								&& self.cacheMoves[item.id].playerId == undefined 
+								&& (self.cacheMoves[item.id].coord1 == undefined || self.cacheMoves[item.id].coord2 == undefined));
+			}
+		);
+		
+		self._findPlayerIdsFromMovements(movementsToCheck);
+		
 		var $table = $("<table>").addClass("MMHKPLUS_Table").appendTo(this.$elem);
 		$(
 			"<tr>\
@@ -120,12 +132,16 @@ MMHKPLUS.Lookout = MMHKPLUS.PanelElement.extend({
                 <th class='MMHKPLUS_CellHeader' style='width:50px;'>" + "" + "</th>\
 			</tr>"
 		).appendTo($table);
+		
 		filterMoves.forEach(function(move)
 			{
-				var playerId = move.masterHeroMove.playerId || 0;
-				var startDate = move.masterHeroMove.creationDate;
+				var playerId = -1.
+				if(hasProperty(self.cacheMoves, move.id)) {
+					playerId = self.cacheMoves[move.id].playerId || -1;
+				}
+				var startDate = move.startDate;
 				var endDate = new Date();
-				endDate.setTime(move.masterHeroMove.endDate * 1000);
+				endDate.setTime(move.endDate * 1000);
 				var from = { x : parseInt(move.masterHeroMove.x1), y : parseInt(move.masterHeroMove.y1) };
 				var to = { x : parseInt(move.masterHeroMove.x2), y : parseInt(move.masterHeroMove.y2) };
 				var speedText = "";
@@ -139,19 +155,22 @@ MMHKPLUS.Lookout = MMHKPLUS.PanelElement.extend({
 				
 				if(endDate.getTime() > $.now())
 				{
-					var data = self._getPlayerInfo(playerId);
+					if(playerId != -1) {
+						var data = self._getPlayerInfo(playerId);
+					}
 
 					var $line = $("<tr>").appendTo($table);
 
 					$("<td>").addClass("MMHKPLUS_Cell clickable")
 						.css({textAlign : "left", paddingLeft : "15px"})
-						.addClass("MMHKPLUS_LookoutAlliance" + data.allianceId)
-						.append(self._createBlockColor(self, playerId) + data.allianceName)
+						.addClass(data ? "MMHKPLUS_LookoutAlliance" + data.allianceId : "")
+						.append(data ? self._createBlockColor(self, playerId) + data.allianceName : "?")
 						.appendTo($line);
+					 
 					$("<td>").addClass("MMHKPLUS_Cell MMHKPLUS_TextCenter clickable")
-						.addClass("MMHKPLUS_LookoutPlayer" + playerId)
-						.click(function() {MMHKPLUS.openPlayerFrame(playerId);})
-						.html(data.playerName)
+						.addClass(data ? "MMHKPLUS_LookoutPlayer" + playerId : "")
+						.click(function() {if(playerId != undefined) MMHKPLUS.openPlayerFrame(playerId);})
+						.html(data ? data.playerName : "?")
 						.appendTo($line);
 					$("<td>").addClass("MMHKPLUS_Cell MMHKPLUS_TextCenter clickable")
 						.addClass("MMHKPLUS_Lookout" + from.x + "_" + from.y)
@@ -171,12 +190,11 @@ MMHKPLUS.Lookout = MMHKPLUS.PanelElement.extend({
 						.html(endDate.toShortFrenchFormat())
 						.appendTo($line);
 					
-					$("<td>").addClass("MMHKPLUS_Cell MMHKPLUS_TextCenter clickable")
+					 $("<td>").addClass("MMHKPLUS_Cell MMHKPLUS_TextCenter clickable")
 						// .css({width : "45px"})
 						.html(move.masterHeroMove.heroId)
-						.click(function() {MMHKPLUS.openDisplayable("AllianceHeroes");MMHKPLUS.getElement("AllianceHeroes",true)._loadHero(playerId,move.masterHeroMove.heroId)})
+						.click(function() {if(data) {MMHKPLUS.openDisplayable("AllianceHeroes");MMHKPLUS.getElement("AllianceHeroes",true)._loadHero(playerId,move.masterHeroMove.heroId); }})
 						.appendTo($line);
-					
 					var diff = move.dur;
 					var s = MMHKPLUS.getElement("Player").get("worldSize");
 					var dx = Math.abs(move.x2p - move.x1p); var dy = Math.abs(move.y2p - move.y1p);
@@ -248,71 +266,73 @@ MMHKPLUS.Lookout = MMHKPLUS.PanelElement.extend({
                                     }))
                         .appendTo($line);
 					
-					var $tooltip = MMHKPLUS.getElement("Tooltip", true).setContent($line, function($container, $tip)
+					 var $tooltip = MMHKPLUS.getElement("Tooltip", true).setContent($line, function($container, $tip)
 						{
 							$tip.css({minWidth : "210px"});
-							var data = self._getPlayerInfo(playerId);
-							if(hasProperty(data, "playerBGNb"))
-							{
-								var $avatar = MMHKPLUS.getPlayerAvatar(data.playerBGNb, data.playerPatternNb, data.playerIconNb)
-									.css( { position : "relative"} )
-									.appendTo($tip);
-								var pName = data.playerName + " (" + data.allianceName + ")";
-								$("<p>")
-									.html(pName)
-									.css(
-										{
-											"position" : "relative",
-											"width" : "150px",
-											"padding-left" : "50px",
-											"top" : "-115px",
-											"font-weight" : "bold"
-										})
-									.appendTo($avatar);
-								
-								$("<p style='margin-top:10px;'/>")
-									.html(MMHKPLUS.localize("DOMINATION") + " : " + data.dominationPosition + " (" + formatNumber(data.dominationScore) + ")")
-									.appendTo($tip);
-								$("<p/>")
-									.html(MMHKPLUS.localize("WEALTH") + " : " + data.wealthPosition+ " (" + formatNumber(data.wealthScore) + ")")
-									.appendTo($tip);
-								$("<p/>")
-									.html(MMHKPLUS.localize("HONOR") + " : " + data.honorPosition+ " (" + formatNumber(data.honorScore) + ")")
-									.appendTo($tip);
-								$("<br/>").appendTo($tip);
-								$("<p/>")
-									.html(MMHKPLUS.localize("CITY_COUNT") + " : " + data.cityList.length)
-									.appendTo($tip);
-								var d = new Date(); var tmp = new Date(); tmp.setTime(move.masterHeroMove.endDate * 1000 - d.getTime());
-                                var tmp2 = new Date(); tmp2.setTime(d.getTime() - move.masterHeroMove.startDate * 1000);
-								var endCD = (d > endDate ? "Terminé" : tmp.countDown());
-                                var startCD = tmp2.countDown();
-								$("<br/>").appendTo($tip);
-								$("<p/>")
-									.html(MMHKPLUS.localize("SPEED") + " : " + speedText)
-									.appendTo($tip);
-
-                                if(move.endDate != move.masterHeroMove.endDate)
-                                {
-                                    // var startHalt = new Date() ; startHalt.setTime(move.startDate * 1000);
-                                    var endHalt = new Date() ; endHalt.setTime(move.endDate * 1000);
-                                    $("<br/>").appendTo($tip);
-                                    // $("<p/>")
-                                    //     .html(MMHKPLUS.localize("START_HALT") + " : "  + startHalt.toShortFrenchFormat())
-                                    //     .appendTo($tip);
-                                    $("<p/>")
-                                        .html((move.x2p == move.x1p && move.y2p == move.y1p ? MMHKPLUS.localize("END_HALT") : MMHKPLUS.localize("START_HALT")) + " : "  + endHalt.toShortFrenchFormat())
-                                        .appendTo($tip);
-                                }
-
-								$("<br/>").appendTo($tip);
-                                $("<p/>")
-                                    .html(MMHKPLUS.localize("STARTED_SINCE") + " : "  + startCD)
-                                    .appendTo($tip);
-								$("<p/>")
-									.html(MMHKPLUS.localize("END_IN") + " : "  + endCD)
-									.appendTo($tip);
+							if(playerId != -1) {
+								var data = self._getPlayerInfo(playerId);
+								if(hasProperty(data, "playerBGNb"))
+								{
+									var $avatar = MMHKPLUS.getPlayerAvatar(data.playerBGNb, data.playerPatternNb, data.playerIconNb)
+										.css( { position : "relative"} )
+										.appendTo($tip);
+									var pName = data.playerName + " (" + data.allianceName + ")";
+									$("<p>")
+										.html(pName)
+										.css(
+											{
+												"position" : "relative",
+												"width" : "150px",
+												"padding-left" : "50px",
+												"top" : "-115px",
+												"font-weight" : "bold"
+											})
+										.appendTo($avatar);
+									
+									$("<p style='margin-top:10px;'/>")
+										.html(MMHKPLUS.localize("DOMINATION") + " : " + data.dominationPosition + " (" + formatNumber(data.dominationScore) + ")")
+										.appendTo($tip);
+									$("<p/>")
+										.html(MMHKPLUS.localize("WEALTH") + " : " + data.wealthPosition+ " (" + formatNumber(data.wealthScore) + ")")
+										.appendTo($tip);
+									$("<p/>")
+										.html(MMHKPLUS.localize("HONOR") + " : " + data.honorPosition+ " (" + formatNumber(data.honorScore) + ")")
+										.appendTo($tip);
+									$("<br/>").appendTo($tip);
+									$("<p/>")
+										.html(MMHKPLUS.localize("CITY_COUNT") + " : " + data.cityList.length)
+										.appendTo($tip);
+								}
 							}
+							var d = new Date(); var tmp = new Date(); tmp.setTime(move.endDate * 1000 - d.getTime());
+                            var tmp2 = new Date(); tmp2.setTime(d.getTime() - move.startDate * 1000);
+							var endCD = (d > endDate ? "Terminé" : tmp.countDown());
+                            var startCD = tmp2.countDown();
+							$("<br/>").appendTo($tip);
+							$("<p/>")
+								.html(MMHKPLUS.localize("SPEED") + " : " + speedText)
+								.appendTo($tip);
+
+                            if(move.endDate != move.endDate)
+                            {
+                                // var startHalt = new Date() ; startHalt.setTime(move.startDate * 1000);
+                                var endHalt = new Date() ; endHalt.setTime(move.endDate * 1000);
+                                $("<br/>").appendTo($tip);
+                                // $("<p/>")
+                                //     .html(MMHKPLUS.localize("START_HALT") + " : "  + startHalt.toShortFrenchFormat())
+                                //     .appendTo($tip);
+                                $("<p/>")
+                                    .html((move.x2p == move.x1p && move.y2p == move.y1p ? MMHKPLUS.localize("END_HALT") : MMHKPLUS.localize("START_HALT")) + " : "  + endHalt.toShortFrenchFormat())
+                                    .appendTo($tip);
+                            }
+
+							$("<br/>").appendTo($tip);
+                            $("<p/>")
+                                .html(MMHKPLUS.localize("STARTED_SINCE") + " : "  + startCD)
+                                .appendTo($tip);
+							$("<p/>")
+								.html(MMHKPLUS.localize("END_IN") + " : "  + endCD)
+								.appendTo($tip);
 							
 							$tip = null;
 							$container = null;
@@ -400,6 +420,80 @@ MMHKPLUS.Lookout = MMHKPLUS.PanelElement.extend({
 		$("div.MMHKPLUS_Color" + data.allianceId).css({backgroundColor : getColor(data.color)});;
 	},
 	
+	_findPlayerIdsFromMovements : function(movements) 
+	{
+		var self = this;
+		var regionList = MMHKPLUS.HOMMK.worldMap.content.attachedRegionList;
+		
+		regionList.forEach(function(r) 
+			{
+				movements.forEach(function(m)
+					{
+						var movementCoordinates = 
+							[
+							 	{x: m.masterHeroMove.x1, y: m.masterHeroMove.y1},
+							 	{x: m.masterHeroMove.x2, y: m.masterHeroMove.y2}
+							]
+						;
+						
+						movementCoordinates.forEach(function(c)
+							{
+								if(r.isC && r.x == c.x && r.y == c.y) {
+									// Region match, check alliance/player color
+									if(hasProperty(r, "_iaCol")) {
+										var color = r._iaCol
+									}
+									else {
+										var color = r._ipCol;
+									}
+									if(color == m.color) {
+										// Put move and player in cache
+										self._putMoveInCache(m.id, undefined, undefined, r.pId);
+										self._getPlayerInfo(r.pId);
+									}
+								}
+								else if(!r.isC && r.x == c.x && r.y == c.y) {
+									// if region is not a city, do not check it again
+									// so we save this information in move cache
+									// and if the two coordinates in move cache are defined,
+									// it means that we do not need to check this move playerId
+									// (typically Black Elves movements)
+									self._putMoveInCache(m.id, c.x, c.y, undefined);
+								}
+							}
+						);
+					}
+				);
+			}
+		);
+	},
+	
+	_putMoveInCache : function(id, x, y, playerId) 
+	{
+		var self = this;
+		if(!hasProperty(self.cacheMoves, id)) {
+			self.cacheMoves[id] =
+				{
+					coord1 : undefined,
+					coord2 : undefined,
+					playerId: playerId
+				}
+			;
+		}
+		if(x != undefined && y != undefined) {
+			var c = {x : x, y : y};
+			if(self.cacheMoves[id].coord1 == undefined) {
+				self.cacheMoves[id].coord1 = c;
+			}
+			else {
+				self.cacheMoves[id].coord2 = c
+			}
+		}
+		if(playerId != undefined) {
+			self.cacheMoves[id].playerId = playerId;
+		}
+	},
+	
 	unload : function()
 	{
 
@@ -408,5 +502,6 @@ MMHKPLUS.Lookout = MMHKPLUS.PanelElement.extend({
 		destroy(this.moves); this.moves = null;
 		destroy(this.cachePlayers); this.cachePlayers = null;
 		destroy(this.cacheAlliances); this.cacheAlliances = null;
+		destroy(this.cacheMoves); this.cacheMoves = null;
 	}
 });
