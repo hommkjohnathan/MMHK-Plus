74b200a645582edfcf866fa6cc8288eac91a5a9c
Aendawyn <aendawyn@gmail.com>
2013-10-28 12:25:04 +0100

    Add MMHK-Plus URL API

--- MMHK-Plus/src/header/mmhkplusHeader.js
+++ MMHK-Plus/src/header/mmhkplusHeader.js
@@ -7,6 +7,7 @@ MMHKPLUS.URL = "${mmhk-plus.website.url}";
 MMHKPLUS.URL_JACTARI = "${mmhk-plus.jactari.website.url}";
 MMHKPLUS.URL_IMAGES = MMHKPLUS.URL + "/script/images/";
 MMHKPLUS.URL_PHP = MMHKPLUS.URL + "/script/php/";
+MMHKPLUS.URL_API = (MMHKPLUS.URL + "/api/v1/").replace("http://www.", "http://"); // Need to remove www since we get a 301 http response with it (?)
 
 MMHKPLUS.elementPool = {};
 MMHKPLUS.resources = [ "GOLD", "WOOD", "ORE", "MERCURY", "CRYSTAL", "SULFUR", "GEM" ];
