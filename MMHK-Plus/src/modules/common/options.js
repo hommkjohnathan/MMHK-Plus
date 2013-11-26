fc9ff2e4598d15298f32f38c0700eb07433a55b3
Aendawyn <aendawyn@gmail.com>
2013-11-26 11:22:44 +0100

    Update Options panel

--- MMHK-Plus/src/modules/common/options.js
+++ MMHK-Plus/src/modules/common/options.js
@@ -7,8 +7,8 @@ MMHKPLUS.Options = MMHKPLUS.PanelElement.extend({
         opened : false,
         x : "center",
         y : "center",
-        w : 250,
-        h : 580,
+        w : 600,
+        h : 300,
         savePos : true,
         saveWidth : false,
         saveHeight : false,
@@ -33,215 +33,207 @@ MMHKPLUS.Options = MMHKPLUS.PanelElement.extend({
 
     _createView : function()
     {
-        var self = this;
-        this.$elem.empty();
-
-        $("<br>").appendTo(this.$elem);
-
-        // Menu type
-        $("<p>").html(MMHKPLUS.localize("MENU_TYPE") + " : ").appendTo(this.$elem);
-        $("<input>").attr("type", "radio")
-            .css("margin-right", "10px")
-            .css("margin-left", "25px")
-            .attr("name", "menuFormat")
-            .attr("value", 0)
-            .attr("checked", MMHKPLUS.getElement("Menu").options.type == 0)
-            .change(function() { MMHKPLUS.getElement("Menu").toggleType();})
-            .appendTo(this.$elem);
-        this.$elem.append(
-            MMHKPLUS.localize("MENU_TOPBAR") + "<br/>");
-        $("<input>").attr("type", "radio")
-            .css("margin-right", "10px")
-            .css("margin-left", "25px")
-            .attr("name", "menuFormat")
-            .attr("value", 1)
-            .attr("checked", MMHKPLUS.getElement("Menu").options.type == 1)
-            .change(function() { MMHKPLUS.getElement("Menu").toggleType();})
-            .appendTo(this.$elem);
-        this.$elem.append(
-            MMHKPLUS.localize("MENU_FLOAT") + "<br/>");
-
-        $("<br>").appendTo(this.$elem);
-
-        // Buyable
-        $("<p>").html(MMHKPLUS.localize("D_BUYABLE") + " : ").appendTo(this.$elem);
-        $("<input>").attr("type", "radio")
-            .css("margin-right", "10px")
-            .css("margin-left", "25px")
-            .attr("name", "displayBuyable")
-            .attr("value", 0)
-            .attr("checked", MMHKPLUS.getElement("EnhancedUI").options.showBuyable == 1)
-            .change(function() { MMHKPLUS.getElement("EnhancedUI").toggleBuyable();})
-            .appendTo(this.$elem);
-        this.$elem.append(
-            MMHKPLUS.localize("YES") + "<br/>");
-        $("<input>").attr("type", "radio")
-            .css("margin-right", "10px")
-            .css("margin-left", "25px")
-            .attr("name", "displayBuyable")
-            .attr("value", 1)
-            .attr("checked", MMHKPLUS.getElement("EnhancedUI").options.showBuyable == 0)
-            .change(function() { MMHKPLUS.getElement("EnhancedUI").toggleBuyable();})
-            .appendTo(this.$elem);
-        this.$elem.append(
-            MMHKPLUS.localize("NO") + "<br/>");
-
-        $("<br>").appendTo(this.$elem);
-
-        // Influence
-        $("<p>").html(MMHKPLUS.localize("D_INFLUENCE") + " : ").appendTo(this.$elem);
-        $("<input>").attr("type", "radio")
-            .css("margin-right", "10px")
-            .css("margin-left", "25px")
-            .attr("name", "displayInfluence")
-            .attr("value", 0)
-            .attr("checked", MMHKPLUS.getElement("EnhancedUI").options.showInfluence == 1)
-            .change(function() { MMHKPLUS.getElement("EnhancedUI").toggleInfluence();})
-            .appendTo(this.$elem);
-        this.$elem.append(
-            MMHKPLUS.localize("YES") + "<br/>");
-        $("<input>").attr("type", "radio")
-            .css("margin-right", "10px")
-            .css("margin-left", "25px")
-            .attr("name", "displayInfluence")
-            .attr("value", 1)
-            .attr("checked", MMHKPLUS.getElement("EnhancedUI").options.showInfluence == 0)
-            .change(function() { MMHKPLUS.getElement("EnhancedUI").toggleInfluence();})
-            .appendTo(this.$elem);
-        this.$elem.append(
-            MMHKPLUS.localize("NO") + "<br/>");
-
-        $("<br>").appendTo(this.$elem);
-
-
-        // Alerts
-        $("<p>").html(MMHKPLUS.localize("D_PANELS") + " : ").appendTo(this.$elem);
-        $("<input>").attr("type", "radio")
-            .css("margin-right", "10px")
-            .css("margin-left", "25px")
-            .attr("name", "displayPanels")
-            .attr("value", 0)
-            .attr("checked", MMHKPLUS.getElement("EnhancedUI").options.showPanels == 1)
-            .change(function() { MMHKPLUS.getElement("EnhancedUI").togglePanels();})
-            .appendTo(this.$elem);
-        this.$elem.append(
-            MMHKPLUS.localize("YES") + "<br/>");
-        $("<input>").attr("type", "radio")
-            .css("margin-right", "10px")
-            .css("margin-left", "25px")
-            .attr("name", "displayPanels")
-            .attr("value", 1)
-            .attr("checked", MMHKPLUS.getElement("EnhancedUI").options.showPanels == 0)
-            .change(function() { MMHKPLUS.getElement("EnhancedUI").togglePanels();})
-            .appendTo(this.$elem);
-        this.$elem.append(
-            MMHKPLUS.localize("NO") + "<br/>");
-
-        $("<br>").appendTo(this.$elem);
-
-        // Movements
-        $("<p>").html(MMHKPLUS.localize("D_MOVEMENTS") + " : ").appendTo(this.$elem);
-        $("<input>").attr("type", "radio")
-            .css("margin-right", "10px")
-            .css("margin-left", "25px")
-            .attr("name", "displayMovements")
-            .attr("value", 0)
-            .attr("checked", MMHKPLUS.getElement("EnhancedUI").options.showMovements == 1)
-            .change(function() { MMHKPLUS.getElement("EnhancedUI").toggleMovements();})
-            .appendTo(this.$elem);
-        this.$elem.append(
-            MMHKPLUS.localize("YES") + "<br/>");
-        $("<input>").attr("type", "radio")
-            .css("margin-right", "10px")
-            .css("margin-left", "25px")
-            .attr("name", "displayMovements")
-            .attr("value", 1)
-            .attr("checked", MMHKPLUS.getElement("EnhancedUI").options.showMovements == 0)
-            .change(function() { MMHKPLUS.getElement("EnhancedUI").toggleMovements();})
-            .appendTo(this.$elem);
-        this.$elem.append(
-            MMHKPLUS.localize("NO") + "<br/>");
-
-        $("<br>").appendTo(this.$elem);
-
-        // Game to left
-        $("<p>").html(MMHKPLUS.localize("D_GAMETOLEFT") + " : ").appendTo(this.$elem);
-        $("<input>").attr("type", "radio")
-            .css("margin-right", "10px")
-            .css("margin-left", "25px")
-            .attr("name", "gameToleft")
-            .attr("value", 0)
-            .attr("checked", MMHKPLUS.getElement("EnhancedUI").options.gameToleft == true)
-            .change(function() { MMHKPLUS.getElement("EnhancedUI").toggleGamePosition();})
-            .appendTo(this.$elem);
-        this.$elem.append(
-            MMHKPLUS.localize("YES") + "<br/>");
-        $("<input>").attr("type", "radio")
-            .css("margin-right", "10px")
-            .css("margin-left", "25px")
-            .attr("name", "gameToleft")
-            .attr("value", 1)
-            .attr("checked", MMHKPLUS.getElement("EnhancedUI").options.gameToleft == false)
-            .change(function() { MMHKPLUS.getElement("EnhancedUI").toggleGamePosition();})
-            .appendTo(this.$elem);
-        this.$elem.append(
-            MMHKPLUS.localize("NO") + "<br/>");
-
-        $("<br>").appendTo(this.$elem);
-
-        // Chat
-        $("<p>").html(MMHKPLUS.localize("U_CHAT") + " : ").appendTo(this.$elem);
-        $("<input>").attr("type", "radio")
-            .css("margin-right", "10px")
-            .css("margin-left", "25px")
-            .attr("name", "chatType")
-            .attr("value", 2)
-            .attr("checked", MMHKPLUS.getElement("EnhancedUI").options.chatType == 2)
-            .mouseup(function() { if($rb2.is(":checked")) { MMHKPLUS.alert(MMHKPLUS.localize("WARNING"), MMHKPLUS.localize("RESTART_GAME")); }})
-            .change(function() { MMHKPLUS.getElement("EnhancedUI").toggleChat(2);})
-            .appendTo(this.$elem);
-        this.$elem.append(
-            MMHKPLUS.localize("U_CHAT1") + "<br/>");
-        var $rb2 = $("<input>").attr("type", "radio")
-            .css("margin-right", "10px")
-            .css("margin-left", "25px")
-            .attr("name", "chatType")
-            .attr("value", 1)
-            .attr("checked", MMHKPLUS.getElement("EnhancedUI").options.chatType == 1)
-            .change(function() { MMHKPLUS.getElement("EnhancedUI").toggleChat(1);})
-            .appendTo(this.$elem);
-        this.$elem.append(
-            MMHKPLUS.localize("U_CHAT2") + "<br/>");
-        $("<input>").attr("type", "radio")
-            .css("margin-right", "10px")
-            .css("margin-left", "25px")
-            .attr("name", "chatType")
-            .attr("value", 0)
-            .mouseup(function() { if($rb2.is(":checked")) { MMHKPLUS.alert(MMHKPLUS.localize("WARNING"), MMHKPLUS.localize("RESTART_GAME")); }})
-            .attr("checked", MMHKPLUS.getElement("EnhancedUI").options.chatType == 0)
-            .change(function() { MMHKPLUS.getElement("EnhancedUI").toggleChat(0);})
-            .appendTo(this.$elem);
-        this.$elem.append(
-            MMHKPLUS.localize("U_CHAT3") + "<br/>");
-
-        $("<br>").appendTo(this.$elem);
-
-        // Space used
-        var store = MMHKPLUS.getElement("Store");
-        $("<p>").html(MMHKPLUS.localize("U_SPACE") + " : ").appendTo(this.$elem);
-        $("<p>")
-            .css("margin-left", "25px")
-            .html(Math.floor(store.spaceUsed()/1000) + "/" + Math.floor(store.options.quota/1000) + " (" + Math.floor(store.spaceUsed() / store.options.quota) + "%)").appendTo(this.$elem);
-        $("<div>").button()
-            .css("padding", "5px")
-            .css("margin-left", "25px")
-            .css("margin-top", "5px")
-            .html(MMHKPLUS.localize("CLEAN"))
-            .click(function()
-                {
-                    self._cleanSpace();
-                })
-            .appendTo(this.$elem);
+		var self = this;
+		this.$elem.empty();
+		
+		var $table = $('<table>')
+			.addClass("MMHKPLUS_100Width")
+			.appendTo(this.$elem);
+        
+		var $rb2 = $("<input>").attr("type", "radio")
+			.css("margin-right", "10px")
+			.css("margin-left", "25px")
+			.attr("name", "chatType")
+			.attr("value", 1)
+			.attr("checked", MMHKPLUS.getElement("EnhancedUI").options.chatType == 1)
+			.change(function() { MMHKPLUS.getElement("EnhancedUI").toggleChat(1);});
+		
+		$("<tr>")
+			.addClass("MMHKPLUS_100Width")
+			.appendTo($table)
+			.append(
+				$("<td>")
+					.css("width", "50%")
+					.append($("<p>").html(MMHKPLUS.localize("MENU_TYPE")))
+					.append(
+						$("<input>").attr("type", "radio")
+				            .css("margin-right", "10px")
+				            .css("margin-left", "25px")
+				            .attr("name", "menuFormat")
+				            .attr("value", 0)
+				            .attr("checked", MMHKPLUS.getElement("Menu").options.type == 0)
+				            .change(function() { MMHKPLUS.getElement("Menu").toggleType();}))
+					.append($("<label>").html(MMHKPLUS.localize("MENU_TOPBAR")))
+					.append($("<br>"))
+					.append(
+						$("<input>").attr("type", "radio")
+				            .css("margin-right", "10px")
+				            .css("margin-left", "25px")
+				            .css("margin-bottom", "2em")
+				            .attr("name", "menuFormat")
+				            .attr("value", 1)
+				            .attr("checked", MMHKPLUS.getElement("Menu").options.type == 1)
+				            .change(function() { MMHKPLUS.getElement("Menu").toggleType();}))
+					.append($("<label>").html(MMHKPLUS.localize("MENU_FLOAT")))
+			)
+			.append(
+				$("<td>")
+					.css("width", "50%")
+					.append($("<p>").html(MMHKPLUS.localize("U_CHAT")))
+					.append(
+						$("<input>").attr("type", "radio")
+				            .css("margin-right", "10px")
+				            .css("margin-left", "25px")
+				            .attr("name", "chatType")
+				            .attr("value", 2)
+				            .attr("checked", MMHKPLUS.getElement("EnhancedUI").options.chatType == 2)
+				            .change(function() { MMHKPLUS.getElement("EnhancedUI").toggleChat(2);}))
+					.append($("<label>").html(MMHKPLUS.localize("U_CHAT1")))
+					.append($("<br>"))
+					.append($rb2)
+					.append($("<label>").html(MMHKPLUS.localize("U_CHAT2")))
+					.append($("<br>"))
+					.append(
+						$("<input>").attr("type", "radio")
+				            .css("margin-right", "10px")
+				            .css("margin-left", "25px")
+				            .attr("name", "chatType")
+				            .attr("value", 0)
+				            .mouseup(function() { if($rb2.is(":checked")) { MMHKPLUS.alert(MMHKPLUS.localize("WARNING"), MMHKPLUS.localize("RESTART_GAME")); }})
+				            .attr("checked", MMHKPLUS.getElement("EnhancedUI").options.chatType == 0)
+				            .change(function() { MMHKPLUS.getElement("EnhancedUI").toggleChat(0);}))
+				    .append($("<label>").html(MMHKPLUS.localize("U_CHAT3")))
+			);
+		
+		$("<tr>")
+			.addClass("MMHKPLUS_100Width")
+			.appendTo($table)
+			.append(
+				$("<td>")
+					.css("width", "50%")
+					.append(
+						$("<input>").attr("type", "checkbox")
+							.css("margin-right", "5px")
+							.attr("checked", MMHKPLUS.getElement("EnhancedUI").options.showBuyable)
+							.change(function() { MMHKPLUS.getElement("EnhancedUI").toggleBuyable();}))
+					.append($("<label>").html(MMHKPLUS.localize("D_BUYABLE")))
+			)
+			.append(
+				$("<td>")
+					.css("width", "50%")
+					.append(
+						$("<input>").attr("type", "checkbox")
+							.css("margin-right", "5px")
+							.attr("checked", MMHKPLUS.getElement("EnhancedUI").options.showInfluence)
+							.change(function() { MMHKPLUS.getElement("EnhancedUI").toggleInfluence();}))
+					.append($("<label>").html(MMHKPLUS.localize("D_INFLUENCE")))
+			);
+						
+		$("<tr>")
+			.addClass("MMHKPLUS_100Width")
+			.appendTo($table)
+			.append(
+				$("<td>")
+					.css("width", "50%")
+					.append(
+						$("<input>").attr("type", "checkbox")
+							.css("margin-right", "5px")
+							.attr("checked", MMHKPLUS.getElement("EnhancedUI").options.showPanels)
+							.change(function() { MMHKPLUS.getElement("EnhancedUI").togglePanels();}))
+					.append($("<label>").html(MMHKPLUS.localize("D_PANELS")))
+			)
+			.append(
+				$("<td>")
+					.css("width", "50%")
+					.append(
+						$("<input>").attr("type", "checkbox")
+							.css("margin-right", "5px")
+							.attr("checked", MMHKPLUS.getElement("EnhancedUI").options.showMovements)
+							.change(function() { MMHKPLUS.getElement("EnhancedUI").toggleMovements();}))
+					.append($("<label>").html(MMHKPLUS.localize("D_MOVEMENTS")))
+			);		
+		
+		$("<tr>")
+			.addClass("MMHKPLUS_100Width")
+			.appendTo($table)
+			.append(
+				$("<td>")
+					.css("width", "50%")
+					.append(
+						$("<input>").attr("type", "checkbox")
+							.css("margin-right", "5px")
+							.attr("checked", MMHKPLUS.getElement("EnhancedUI").options.gameToleft)
+							.change(function() { MMHKPLUS.getElement("EnhancedUI").toggleGamePosition();}))
+					.append($("<label>").html(MMHKPLUS.localize("D_GAMETOLEFT")))
+			)
+			.append(
+				$("<td>")
+					.css("width", "50%")
+					.append(
+						$("<input>").attr("type", "checkbox")
+							.css("margin-right", "5px")
+							.attr("checked", MMHKPLUS.getElement("EnhancedUI").options.showDistances)
+							.change(function() { MMHKPLUS.getElement("EnhancedUI").toggleDistances();}))
+					.append($("<label>").html(MMHKPLUS.localize("D_DISTANCES")))
+			);
+		
+		$("<tr>")
+			.addClass("MMHKPLUS_100Width")
+			.appendTo($table)
+			.append(
+				$("<td>")
+					.css("width", "50%")
+					.append(
+						$("<input>").attr("type", "checkbox")
+							.css("margin-right", "5px")
+							.attr("checked", MMHKPLUS.getElement("EnhancedUI").options.showRequestIndicator)
+							.change(function() { MMHKPLUS.getElement("EnhancedUI").toggleRequestIndicator();}))
+					.append($("<label>").html(MMHKPLUS.localize("D_REQUEST")))
+			)
+			.append(
+				$("<td>")
+					.css("width", "50%")
+					.append(
+						$("<input>").attr("type", "checkbox")
+							.css("margin-right", "5px")
+							.attr("checked", MMHKPLUS.getElement("EnhancedUI").options.usePlayerChatColor)
+							.change(function() { MMHKPLUS.getElement("EnhancedUI").togglePlayerChatColor();}))
+					.append($("<label>").html(MMHKPLUS.localize("D_PLAYERCHAT")))
+			);		
+		
+		var store = MMHKPLUS.getElement("Store");
+		
+		$("<tr>")
+			.addClass("MMHKPLUS_100Width")
+			.appendTo($table)
+			.append(
+				$("<td>")
+					.css("width", "50%")
+					.append(
+						$("<input>").attr("type", "checkbox")
+							.css("margin-right", "5px")
+							.attr("checked", MMHKPLUS.getElement("EnhancedUI").options.showResources)
+							.change(function() { MMHKPLUS.getElement("EnhancedUI").toggleResources();}))
+					.append($("<label>").html(MMHKPLUS.localize("D_RESOURCES")))
+			);
+		
+		$("<br>").appendTo(this.$elem);
+		$("<div>")
+			.addClass("MMHKPLUS_100Width MMHKPLUS_TextCenter")
+			.appendTo(this.$elem)
+			.append(
+				$("<p>")
+					.addClass("MMHKPLUS_AutoCenter")
+					.html(MMHKPLUS.localize("U_SPACE") + ": " +Math.floor(store.spaceUsed()/1000) + "/" + Math.floor(store.options.quota/1000) + " (" + Math.floor(store.spaceUsed() / store.options.quota) + "%)"))
+			.append(
+				$("<div>").button()
+					.addClass("MMHKPLUS_AutoCenter")
+		            .css("padding", "5px")
+		            .html(MMHKPLUS.localize("CLEAN"))
+		            .click(function()
+		                {
+		                    self._cleanSpace();
+		                }));
     },
 
     _cleanSpace : function()
